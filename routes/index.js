2712964104f60f54a3631ccd21bb85b8ec109423
naveenzerobugz@gmail.com.com <naveenzerobugz@hotmail.com>
2020-10-06 13:35:14 +0530

    test

--- routes/index.js
+++ routes/index.js
@@ -4,7 +4,7 @@ var router = express();
 const eventcreation = require('../controllers/eventcreation');
 
 router.post('/eventcreation', eventcreation.insert);
-router.get('/findevent/:eventname', eventcreation.eventlist);
+router.post('/findevent', eventcreation.eventlist);
 
 router.get('/testing', function(req, res) {
     return res.json('test app working successfully');
